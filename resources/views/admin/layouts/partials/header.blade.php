5109ad756f39ba83c0a07b1b739a96feaf3c7322
baocter2004 <be93700@gmail.com>
2026-04-02 16:14:31 +0700

    Sidebar For Admin Project

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -1,4 +1,4 @@
-<header class="flex justify-between items-center px-4 py-5 bg-white shadow-md">
+<header class="flex justify-between items-center px-6 py-5 bg-white shadow-md">
     <button type="button" id="sidebarToggle"
         class="navbar-btn flex items-center gap-2 text-gray-700 focus:outline-none cursor-pointer">
         <i class="fa-solid fa-bars text-lg"></i>
--- resources/views/admin/layouts/partials/sidebar.blade.php
+++ resources/views/admin/layouts/partials/sidebar.blade.php
@@ -1,5 +1,33 @@
-<aside
-    class="bg-foreground"
-    id="sidebar">
-
+<aside class="bg-foreground" id="sidebar">
+    <div class="header-sidebar">
+        <h1 class="header-sidebar-title">Alibubu</h1>
+        <span class="header-sidebar-note">ENTERPRISE ADMIN</span>
+    </div>
+    <ul class="w-full px-3 space-y-2">
+        <li class="sidebar-items @if (Route::is('admin.dashboard')) active @endif">
+            <a href="{{ route('admin.dashboard') }}"
+                class="sidebar-link {{ request()->routeIs('admin.dashboard') ? 'active' : '' }}">
+                <i class="fa-solid text-lg fa-chart-bar"></i>
+                Dashboard
+            </a>
+        </li>
+        <li class="sidebar-items">
+            <a href="" class="sidebar-link {{ request()->routeIs('admin.products.*') ? 'active' : '' }}">
+                <i class="fa-solid text-lg fa-box"></i>
+                Products
+            </a>
+        </li>
+        <li class="sidebar-items">
+            <a href="" class="sidebar-link {{ request()->routeIs('admin.orders.*') ? 'active' : '' }}">
+                <i class="fa-solid text-lg fa-cart-shopping"></i>
+                Orders
+            </a>
+        </li>
+        <li class="sidebar-items">
+            <a href="" class="sidebar-link {{ request()->routeIs('admin.customers.*') ? 'active' : '' }}">
+                <i class="fa-solid text-lg fa-users"></i>
+                Customers
+            </a>
+        </li>
+    </ul>
 </aside>
